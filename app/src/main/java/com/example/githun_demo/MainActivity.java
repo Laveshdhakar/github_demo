867f406b362926ec5f4d195490b5008d9fd24809
laveshdhakar <laveshdhakar@gmail.com>
2024-06-21 20:35:28 +0530

    This is my second commmit

--- app/src/main/java/com/example/githun_demo/MainActivity.java
+++ app/src/main/java/com/example/githun_demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.githun_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getData();
     }
-}
+
+
+    public void getData(){
+        Toast.makeText(this, "This is my first time trying github ", Toast.LENGTH_SHORT).show();
+    }
+
+} 
